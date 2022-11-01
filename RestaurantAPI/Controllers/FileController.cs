2abc2bffc28e096076034e59488b68018cf6a80d
piotryfon <31383071+piotryfon@users.noreply.github.com>
2022-11-01 18:23:18 +0100

    Zwracanie plików statycznych

---
+++ RestaurantAPI/Controllers/FileController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using System.IO;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("file")]
+    [Authorize]
+    public class FileController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetFile([FromQuery]string fileName)
+        {
+            var rootPath = Directory.GetCurrentDirectory();
+            var filePath = $"{rootPath}/PrivateFiles/{fileName}";
+            var fileExist = System.IO.File.Exists(filePath);
+
+            if (!fileExist)
+            {
+                return NotFound();
+            }
+
+            var contentProvider = new FileExtensionContentTypeProvider();
+            contentProvider.TryGetContentType(filePath, out string contentType);
+
+            var fileContents = System.IO.File.ReadAllBytes(filePath);
+
+            return File(fileContents, contentType, fileName);
+        }
+    }
+}
